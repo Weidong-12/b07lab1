92f45197534f67d25e3e6f15ed989f819d3c684d
Weidong Guo <weidong.guo12@gmail.com>
2025-09-28 22:15:34 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,50 @@
+import java.io.*;
+
 public class Driver {
-public static void main(String [] args) {
-Polynomial p = new Polynomial();
-System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
-Polynomial s = p1.add(p2);
-System.out.println("s(0.1) = " + s.evaluate(0.1));
-if(s.hasRoot(1))
-System.out.println("1 is a root of s");
-else
-System.out.println("1 is not a root of s");
-}
+    public static void main(String[] args) {
+        try {
+
+            double[] c1 = {6, 0, 0, 5};    // 6 + 5x^3
+            int[] e1 = {0, 1, 2, 3};
+            Polynomial p1 = new Polynomial(c1, e1);
+
+            double[] c2 = {0, -2, 0, 0, -9}; // -2x - 9x^4
+            int[] e2 = {0, 1, 2, 3, 4};
+            Polynomial p2 = new Polynomial(c2, e2);
+
+            System.out.println("p1: " + p1);
+            System.out.println("p2: " + p2);
+
+
+            Polynomial sum = p1.add(p2);
+            System.out.println("p1 + p2 = " + sum);
+            System.out.println("sum(0.1) = " + sum.evaluate(0.1));
+
+
+            if (sum.hasRoot(1)) {
+                System.out.println("1 is a root of sum");
+            } else {
+                System.out.println("1 is not a root of sum");
+            }
+
+
+            Polynomial product = p1.multiply(p2);
+            System.out.println("p1 * p2 = " + product);
+            System.out.println("product(0.1) = " + product.evaluate(0.1));
+
+
+            String filename = "poly.txt";
+            sum.saveToFile(filename);
+            System.out.println("Sum polynomial saved to " + filename);
+
+
+            Polynomial fromFile = new Polynomial(new File(filename));
+            System.out.println("Polynomial read from file: " + fromFile);
+
+        } catch (IOException e) {
+            System.out.println("IO Error: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Illegal Argument: " + e.getMessage());
+        }
+    }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,53 +1,184 @@
-public class Polynomial{
-	private double[] coef;
-	
-	public Polynomial(){
-		coef = new double[]{0};
-	}
-	
-	public Polynomial(double[] array){
-		coef = new double[array.length];	
-		for(int i = 0; i<array.length; i++ ){
-			coef[i] = array[i];
-		}
-	}
+import java.io.*;
+import java.util.*;
 
-	public Polynomial add(Polynomial a){
-		int len;
-		double b , c;
-		if(coef.length > a.coef.length){
-			len = coef.length;
-		}else{
-			len = a.coef.length;
-		}
-		double[] result = new double[len];
-		
-		for (int i=0; i<len; i++){
-            if (i < coef.length) {
-                b = coef[i];
-            } else {
-            b = 0;
+public class Polynomial {
+    private double[] coef;
+    private int[] exp;     
+
+
+    public Polynomial() {
+        coef = new double[]{0};
+        exp = new int[]{0};
+    }
+
+
+    public Polynomial(double[] a, int[] b) {
+        if (a.length != b.length) {
+            throw new IllegalArgumentException("Coefficient and exponent arrays must have the same length");
+        }
+
+        List<Double> aList = new ArrayList<>();
+        List<Integer> bList = new ArrayList<>();
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != 0) {
+                aList.add(a[i]);
+                bList.add(b[i]);
             }
-			if (i < a.coef.length) {
-                c = a.coef[i];
+        }
+
+        coef = new double[aList.size()];
+        exp = new int[bList.size()];
+        for (int i = 0; i < aList.size(); i++) {
+            coef[i] = aList.get(i);
+            exp[i] = bList.get(i);
+        }
+    }
+
+
+    public Polynomial(File file) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader(file));
+        String line = f.readLine();
+        f.close();
+        getPolynomial(line);
+    }
+
+
+    private void getPolynomial(String p) {
+        p = p.replace("-", "+-");
+        if (p.startsWith("+-")) p = p.substring(1);
+        else if (p.startsWith("+")) p = p.substring(1);
+
+        String[] terms = p.split("\\+");
+        List<Double> lc = new ArrayList<>();
+        List<Integer> le = new ArrayList<>();
+
+        for (String term : terms) {
+            if (term.isEmpty()) continue;
+
+            double c;
+            int e;
+            if (term.contains("x")) {
+                String[] parts = term.split("x");
+                if (parts[0].equals("") || parts[0].equals("+")) {
+                    c = 1;
+                } else if (parts[0].equals("-")) {
+                    c = -1;
+                } else {
+                    c = Double.parseDouble(parts[0]);
+                }
+                if (parts.length == 1 || parts[1].isEmpty()) {
+                    e = 1;
+                } else {
+                    e = Integer.parseInt(parts[1]);
+                }
             } else {
-            c = 0;
+                c = Double.parseDouble(term);
+                e = 0;
             }
-			result[i] = b+c;
-		}
-		return new Polynomial(result);
-	}
-	
-	public double evaluate(double x){
-		int len = coef.length;
-		double result = 0;
-		for(int i = 0; i < len; i++){
-			result = result + coef[i] * Math.pow(x , i);
-		}
-		return result; 
-	}
-	
-	public boolean hasRoot(double x){
-		return evaluate(x)==0;
-	}
+            lc.add(c);
+            le.add(e);
+        }
+
+        coef = new double[lc.size()];
+        exp = new int[le.size()];
+        for (int i = 0; i < lc.size(); i++) {
+            coef[i] = lc.get(i);
+            exp[i] = le.get(i);
+        }
+    }
+
+
+    public Polynomial add(Polynomial a) {
+        Map<Integer, Double> map = new HashMap<>();
+        for (int i = 0; i < coef.length; i++) {
+            map.put(exp[i], map.getOrDefault(exp[i], 0.0) + coef[i]);
+        }
+        for (int i = 0; i < a.coef.length; i++) {
+            map.put(a.exp[i], map.getOrDefault(a.exp[i], 0.0) + a.coef[i]);
+        }
+        return fromMap(map);
+    }
+
+
+    public Polynomial multiply(Polynomial a) {
+        Map<Integer, Double> map = new HashMap<>();
+        for (int i = 0; i < coef.length; i++) {
+            for (int j = 0; j < a.coef.length; j++) {
+                int newExp = exp[i] + a.exp[j];
+                double newCoef = coef[i] * a.coef[j];
+                map.put(newExp, map.getOrDefault(newExp, 0.0) + newCoef);
+            }
+        }
+        return fromMap(map);
+    }
+
+
+    public double evaluate(double x) {
+        double result = 0;
+        for (int i = 0; i < coef.length; i++) {
+            result += coef[i] * Math.pow(x, exp[i]);
+        }
+        return result;
+    }
+
+
+    public boolean hasRoot(double x) {
+        return Math.abs(evaluate(x)) < 1e-9;
+    }
+
+
+    public void saveToFile(String filename) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
+        bw.write(toString());
+        bw.close();
+    }
+
+
+    private static Polynomial fromMap(Map<Integer, Double> map) {
+        List<Integer> eList = new ArrayList<>(map.keySet());
+        Collections.sort(eList);
+
+        List<Double> cList = new ArrayList<>();
+        List<Integer> exps = new ArrayList<>();
+        for (int e : eList) {
+            double c = map.get(e);
+            if (c != 0) {
+                cList.add(c);
+                exps.add(e);
+            }
+        }
+
+        double[] cArr = new double[cList.size()];
+        int[] eArr = new int[exps.size()];
+        for (int i = 0; i < cList.size(); i++) {
+            cArr[i] = cList.get(i);
+            eArr[i] = exps.get(i);
+        }
+        return new Polynomial(cArr, eArr);
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < coef.length; i++) {
+            double c = coef[i];
+            int e = exp[i];
+            if (c == 0) continue;
+
+            if (sb.length() > 0 && c > 0) sb.append("+");
+
+            if (e == 0) sb.append(c);
+            else if (e == 1) {
+                if (c == 1) sb.append("x");
+                else if (c == -1) sb.append("-x");
+                else sb.append(c + "x");
+            } else {
+                if (c == 1) sb.append("x" + e);
+                else if (c == -1) sb.append("-x" + e);
+                else sb.append(c + "x" + e);
+            }
+        }
+        return sb.toString();
+    }
 }
